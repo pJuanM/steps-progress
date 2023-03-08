446810bcc9d31d0ae0f60cac20b82827b54fc591
Juan <105320041+pJuanM@users.noreply.github.com>
2023-03-08 17:01:50 -0300

    testando mudanças

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const slidePage = document.querySelector(".slide-page");
-const firtNextBtn = document.querySelector(".next")
+const firtNextBtn = document.querySelector(".next")//Próx 2
 
-const prevSecBtn = document.querySelector(".prev-1")
-const nextSecBtn = document.querySelector(".next-1")
+const prevSecBtn = document.querySelector(".prev-1") //Antes 1
+const nextSecBtn = document.querySelector(".next-1") //Próx 3
 
-const prevThirdBtn = document.querySelector(".prev-2")
-const nextThirdBtn = document.querySelector(".next-2")
+const prevThirdBtn = document.querySelector(".prev-2") //Antes 2
+const nextThirdBtn = document.querySelector(".next-2") //Próx 4
 
-const prevFourthBtn = document.querySelector(".prev-3")
-const submitBtn = document.querySelector(".submit")
+const prevFourthBtn = document.querySelector(".prev-3") //Antes 3
+const submitBtn = document.querySelector(".submit") //Enviar
 
 const progressText = document.querySelectorAll(".step p")
 
@@ -53,9 +53,6 @@ submitBtn.addEventListener("click", function() {
     location.reload()
   }, 100)
 })
-
-
-
 prevSecBtn.addEventListener("click", function() {
   slidePage.style.marginLeft = "0%"
   bullet[current - 2].classList.remove("active")
@@ -78,3 +75,56 @@ prevFourthBtn.addEventListener("click", function() {
   progressCheck[current - 2].classList.remove("active")
   current -= 1
 })
+
+
+function page1() {
+ 
+  const firstName = document.querySelector(".firstName").value
+  const lastName = document.querySelector(".lastName").value
+ 
+  if(firstName != "" && lastName != "" ) {
+    document.querySelector(".firstNext").disabled = false
+    return
+  }
+  document.querySelector(".firstNext").disabled = true
+}
+
+
+function page2() {
+
+  const email = document.querySelector(".email").value
+  const phoneNumber = document.querySelector(".phone-number").value
+
+  if(email != '' && phoneNumber != "") {
+    document.querySelector(".next-1").disabled = false
+    return
+  }
+  document.querySelector(".next-1").disabled = true
+}
+
+function page3() {
+  
+    const nasc = document.querySelector(".nasc").value
+
+  
+    if(nasc != "") {
+      document.querySelector(".next-2").disabled = false
+      return
+    }
+    document.querySelector(".next-2").disabled = true
+
+}
+
+function page4() {
+  
+  const username = document.querySelector(".username").value
+  const password = document.querySelector(".password").value
+
+
+  if(username != "" && password != "") {
+    document.querySelector(".submit").disabled = false
+    return
+  }
+  document.querySelector(".submit").disabled = true
+
+}
